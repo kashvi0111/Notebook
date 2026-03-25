a1d006191845146c145336e58a1fa6ed8a23f7cb
kashvi0111 <kashvineerajdua@gnail.com>
2026-03-25 11:35:14 +0530

    Fixes #15 - Find Unique Elements of an array

--- UniqueElements.java
+++ UniqueElements.java
@@ -1,7 +1,36 @@
 //import java.util.Scanner;
 import java.util.*;
 public class UniqueElements {
-    public static void main(String[] args) {
+    static void UniqueElementsanyOrder(int [] arr){
+        // in any order
+        HashSet<Integer> set = new HashSet<> ();
+        for (int i=0;i<arr.length;i++){
+            set.add(arr[i]);
+        }
+
+        System.out.println("Unique Elements are (in any order):" + set);
+
+
+    }
+    // same order
+    static void UniqueElemntssameorder(int[]arr){
+         HashSet<Integer> set = new HashSet<> ();
+         System.out.print("Unique Elements (same order): ");
+         for (int i=0;i<arr.length;i++){
+            if (!set.contains(arr[i])){
+                set.add(arr[i]);
+                System.out.print(arr[i]+" ");
+            }
+        }
+
+
+    }
+    
+
+        
+
+
+        public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         System.out.println("Enter number of Elements:");
         int n = sc.nextInt();
@@ -11,23 +40,11 @@ public class UniqueElements {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-
-        // in any order
-        HashSet<Integer> set = new HashSet<> ();
-        for (int i=0;i<arr.length;i++){
-            set.add(arr[i]);
-        }
-
-        System.out.println("Unique Elements are (in any order):" + set);
-
-        // in same order 
-        LinkedHashSet<Integer> orderedset = new LinkedHashSet<>(); 
-        for (int i=0;i<arr.length;i++){
-            orderedset.add(arr[i]);
-        }
-        System.out.println("Unique Elements are (in orederd form) :" + orderedset);
-
+        UniqueElementsanyOrder(arr);
+        UniqueElemntssameorder(arr);
         sc.close();
+
+        
 
     }
 }
